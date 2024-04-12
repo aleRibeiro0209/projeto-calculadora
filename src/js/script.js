cfe74e33acd4992000b57e49bdf55363f44239c4
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-12 00:01:14 -0300

    Update of the functionaty

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,52 @@
-const entrada = document.querySelectorAll('li')
-const saida = document.getElementById('result').innerText
+const entrada = document.querySelector('.teclado')
+const saida = document.querySelector('.result')
 
-console.log(entrada);
-console.log(saida);
+entrada.addEventListener('click', e => {
+    if (e.target.matches('li')) {
+        
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const mudeSaida = saida.textContent
 
-var cont = 0
-var listaConteudo = []
+        
+        // Se a tecla clicada nao tiver um action significa que e um numero
+        if (!action) {
+            console.log('Numero!')
 
-entrada.forEach(li => {
-    listaConteudo.push(li.innerHTML)
-    cont++
-});
+            // Se o valor na saida for 0 substitui, se nao concatena o que tinha antes mais o novo valor
+            if (mudeSaida === '0') {
+                saida.textContent = keyContent
+            } else {
+                saida.textContent = mudeSaida + keyContent
+            }
+          }
+        
+        // Se a tecla clicada tiver uma acao de operacao matematica, sabemos que e um operedor
+        if ( action === 'add' ||
+             action === 'subtract' ||
+             action === 'multiply' ||
+             action === 'divide' )
+        {
+            key.classList.add('selected')
+            console.log('Operator matematico!')
+        }
 
-console.log(listaConteudo[9])
+        // Se a tecla clicada tiver alguma dessas acoes entao imprimi no console a acao seleciona
+        if (action === 'decimal') {
+            saida.textContent = mudeSaida + '.'
+            console.log('Decimal!')
+          } // Decimal
 
-console.log(cont)
+        if (action === 'clear') {
+            saida.textContent = 0
+            console.log('Limpar!')
+        } // Limpa 
+
+        if (action === 'calculate') {
+            console.log('Igual!')
+        } // Igual
+
+    }
+})
+
